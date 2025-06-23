21d327e55caa0d1de1db385c8220d46010d01021
unknown <ABC@DESKTOP-ST1VH5S.Innovative.net>
2025-06-23 18:36:40 +0500

    Close Booking Renew Bug

--- app/Http/Controllers/Booker/PaymentController.php
+++ app/Http/Controllers/Booker/PaymentController.php
@@ -94,12 +94,13 @@ class PaymentController extends Controller
                     'receipt' => $imagePath,
                 ]);
 
-                BookingPaymentHistory::create([
-                    'booking_id' => $request['booking_id'],
-                    'payment_id' => $payment->id,
-                    'payment_method_id' => $request['payment_method'],
-                    'paid_amount' => $request['amount_receive'],
-                ]);
+                // BookingPaymentHistory::create([
+                //     'booking_id' => $request['booking_id'],
+                //     'payment_id' => $payment->id,
+                //     'payment_method_id' => $request['payment_method'],
+                //     'paid_amount' => $request['amount_receive'],
+                // ]);
+                // dd($request->all());
 
                 $paymentDataMap = [];
                 foreach ($request['invoice_id'] as $key => $invoice_ids) {
@@ -127,7 +128,6 @@ class PaymentController extends Controller
                         ]);
                     }
                 }
-                dd($request->all());
 
                 $paymentDataList = PaymentData::with('invoice')
                     ->where('payment_id', $payment->id)
