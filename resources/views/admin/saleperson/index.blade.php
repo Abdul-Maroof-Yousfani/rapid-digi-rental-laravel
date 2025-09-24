aa292aa782932d0feeb28647dc79bfa80979ed7a
Muhammad Haziq <muhammadhaziq.me@gmail.com>
2025-09-24 01:00:15 -0700

    hide

--- resources/views/admin/saleperson/index.blade.php
+++ resources/views/admin/saleperson/index.blade.php
@@ -11,7 +11,7 @@
                   <div class="card-body">
                     <div class="d-flex justify-content-between align-items-center">
                         <h3 class="mb-0">Sale Person List</h3>
-                        @can('create sale men')
+                        {{-- @can('create sale men') --}}
                             <span>
                                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#createSaleManModal">
                                     Add Salesperson
@@ -20,7 +20,7 @@
                                     Sync From Zoho
                                 </a>
                             </span>
-                        @endcan
+                        {{-- @endcan --}}
                     </div>
                   </div>
                 </div>
