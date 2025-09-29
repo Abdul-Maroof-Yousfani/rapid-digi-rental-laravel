410076c526f8724479d0f2b7e99c2044b1facd70
Muhammad Haziq <muhammadhaziq.me@gmail.com>
2025-09-29 02:14:17 -0700

    cutomer name

--- resources/views/ajaxview/payment-view.blade.php
+++ resources/views/ajaxview/payment-view.blade.php
@@ -2,7 +2,7 @@
 @foreach ($payment as $item)
     <tr>
         <td>{{ $number }}.</td>
-        <td>{{ $item->booking->customer->customer_name }}</td>
+        <td>{{ $item->booking->customer->customer_name ?? 'No Customer'}}</td>
         <td>{{ $item->booking->agreement_no }}</td>
         <td>{{ $item->paymentMethod->name }}</td>
         <td>{{ number_format($item->booking_amount, 2) }}</td>
